e6468f4219d0246cdf4b82d8125c7d9e75fc73fd
John Netzel <johnrnetzel@gmail.com>
2022-04-02 00:05:40 -0800

    Add response and evaluation of user click input on presented choices

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,4 +1,5 @@
 const quizQuestionElement = document.querySelector("#quiz-question");
+const choicesElement = document.querySelector("#choices")
 const choiceAElement = document.querySelector("#choice-a-text");
 const choiceBElement = document.querySelector("#choice-b-text");
 const choiceCElement = document.querySelector("#choice-c-text");
@@ -22,4 +23,26 @@ function loadTestQuestion()
     choiceBElement.textContent = testQuestion.choiceB;
     choiceCElement.textContent = testQuestion.choiceC;
     choiceDElement.textContent = testQuestion.choiceD;
+}
+
+choicesElement.addEventListener("click", function(event)
+{
+    var selectedChoiceElement = event.target;
+    if(selectedChoiceElement.matches("div"))
+    {
+        selectChoice(selectedChoiceElement.dataset.choice);
+    }
+});
+
+function selectChoice(choice)
+{
+    alert("You selected choice " + choice + "\nThe correct choice was " + testQuestion.correctChoice);
+    if(choice == testQuestion.correctChoice)
+    {
+        alert("You were correct!");
+    }
+    else
+    {
+        alert("You were wrong!!!");
+    }
 }
