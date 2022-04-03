e24dc3187c342e6b894cc04aaba7f94494b27db3
John Netzel <johnrnetzel@gmail.com>
2022-04-03 14:48:23 -0800

    Add table display to score page

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,5 +1,5 @@
 var finalScore = 0;
 
-finalScore = window.sessionStorage.getItem("FinalScore");
+finalScore = window.sessionStorage.getItem("FinalScore") || 0.5;
 alert("Your final score is: " + finalScore);
 window.sessionStorage.removeItem("FinalScore");
