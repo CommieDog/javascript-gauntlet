713b3ecf6c0d62a4ae3b8a7dce5456501f3c3f71
John Netzel <johnrnetzel@gmail.com>
2022-04-04 20:39:18 -0800

    Reorganize and comment JS code

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -10,19 +10,39 @@ const previousChoiceFeedbackElement = document.querySelector("#previous-choice-f
 
 var time = 50;
 var score = 0;
+var quizTimeIntervalId;
 var feedbackTimerId;
 
-//questionBank.getNextQuestion();
-updateQuestionElements();
-updateScoreElement();
-var quizTimeIntervalId = window.setInterval(function()
+/**
+ * Initializes page for giving quiz
+ */
+function init()
 {
-    time--;
+    updateQuestionElements();
+    updateScoreElement();
     updateTimeElement();
-    checkTime();
-}, 1000);
-updateTimeElement();
+    setQuizTimer();
+}
 
+/**
+ * Updates score display
+ */
+function updateScoreElement()
+{
+    scoreValueElement.textContent = score;
+}
+
+/**
+ * Updates time display
+ */
+function updateTimeElement()
+{
+    timeValueElement.textContent = time;
+}
+
+/**
+ * Updates question and answer display
+ */
 function updateQuestionElements()
 {
     quizQuestionElement.textContent = questionBank.currentQuestion.questionText;
@@ -32,60 +52,10 @@ function updateQuestionElements()
     choiceDElement.textContent = questionBank.currentQuestion.choiceD;
 }
 
-choicesElement.addEventListener("click", function(event)
-{
-    var selectedChoiceElement = event.target;
-    selectedChoiceElement = getContainingDivElement(selectedChoiceElement)
-    if(selectedChoiceElement.matches("div"))
-    {
-        selectChoice(selectedChoiceElement.dataset.choice);
-    }
-
-    function getContainingDivElement(element)
-    {
-        if(element.matches("div"))
-        {
-            return element;
-        }
-        if(element === choicesElement)
-        {
-            return null;
-        }
-        return getContainingDivElement(element.parentNode);
-    }
-});
-
-function selectChoice(choice)
-{
-    var choseCorrectly = choice == questionBank.currentQuestion.correctChoice;
-    if(choseCorrectly)
-    {
-        score++;
-        updateScoreElement();
-        previousChoiceFeedbackElement.textContent = "Correct!";
-        previousChoiceFeedbackElement.className = "correct";
-    }
-    else
-    {
-        applyTimePenalty();
-        previousChoiceFeedbackElement.textContent = "Wrong!";
-        previousChoiceFeedbackElement.className = "incorrect";
-    }
-    updateFeedbackElement(choseCorrectly);
-    questionBank.selectNextQuestion();
-    updateQuestionElements();
-}
-
-function updateScoreElement()
-{
-    scoreValueElement.textContent = score;
-}
-
-function updateTimeElement()
-{
-    timeValueElement.textContent = time;
-}
-
+/**
+ * Updates feedback display based on correctness of the user's answer
+ * @param {*} correct whether the user answered the last question correctly or not
+ */
 function updateFeedbackElement(correct)
 {
     if(correct)
@@ -98,22 +68,30 @@ function updateFeedbackElement(correct)
         previousChoiceFeedbackElement.textContent = "Wrong!";
         previousChoiceFeedbackElement.className = "incorrect";
     }
-    clearTimeout(feedbackTimerId); // If the timer hasn't expired because the user answered the next question too quickly, go ahead and cancel the last timer
+    clearTimeout(feedbackTimerId); // If the timer hasn't expired yet because the user answered the next question too quickly, go ahead and cancel the last timer
     feedbackTimerId = setTimeout(function()
     {
         previousChoiceFeedbackElement.textContent = "";
     }, 1000); // And set a new one
 }
 
-function applyTimePenalty()
+/**
+ * Starts the interval that counts down the time left on the quiz
+ */
+function setQuizTimer()
 {
-    time -= 4;
-    time = Math.max(0, time); // Don't allow negative values
-    updateTimeElement();
-    checkTime();
+    quizTimeIntervalId = window.setInterval(function()
+    {
+        time--;
+        updateTimeElement();
+        checkTimeRemaining();
+    }, 1000);
 }
 
-function checkTime()
+/**
+ * Checks the remaining time to see if the quiz should be stopped and stops it if time is up
+ */
+function checkTimeRemaining()
 {
     if(time <= 0)
     {
@@ -122,3 +100,81 @@ function checkTime()
         window.location.href = "score.html";
     }
 }
+
+/**
+ * Add a click event listener to the <div> hosting all of the possible clickable answers on the page
+ * The event listener is placed here but delegated to the element that was actually clicked on
+ */
+choicesElement.addEventListener("click", function(event)
+{
+    var userChoice = getUserChoice(event.target);
+    processUserChoice(userChoice);
+
+    /**
+     * Digs through the DOM to grab the user choice code represented in a given element
+     * @param {*} targetElement the element whose corresponding user choice index to look up
+     * @returns the user choice code
+     */
+    function getUserChoice(targetElement)
+    {
+        targetElement = getContainingDivElement(targetElement);
+        return targetElement.dataset.choice;
+
+        /**
+         * Climbs up through the DOM tree to get the closest containing <div> element. This is useful
+         * as the closest <div> is expected to have the user choice index assigned to it
+         * @param {*} element the element where to start the search
+         * @returns 
+         */
+        function getContainingDivElement(element)
+        {
+            if(element.matches("div"))
+            {
+                return element;
+            }
+            if(element === choicesElement)
+            {
+                return null;
+            }
+            return getContainingDivElement(element.parentNode);
+        }
+    }
+
+    /**
+     * Check to see if an answer choice is correct and acts accordingly
+     * @param {*} choice the user's choice index
+     */
+    function processUserChoice(choice)
+    {
+        var choseCorrectly = (choice == questionBank.currentQuestion.correctChoice);
+        if(choseCorrectly)
+        {
+            score++;
+            updateScoreElement();
+            previousChoiceFeedbackElement.textContent = "Correct!";
+            previousChoiceFeedbackElement.className = "correct";
+        }
+        else
+        {
+            applyTimePenalty();
+            previousChoiceFeedbackElement.textContent = "Wrong!";
+            previousChoiceFeedbackElement.className = "incorrect";
+        }
+        updateFeedbackElement(choseCorrectly);
+        questionBank.selectNextQuestion();
+        updateQuestionElements();
+    }
+
+    /**
+     * Deducts 4 seconds from the user's remaining time
+     */
+    function applyTimePenalty()
+    {
+        time -= 4;
+        time = Math.max(0, time); // Don't allow negative values
+        updateTimeElement();
+        checkTimeRemaining();
+    }
+});
+
+init(); // Get the ball rolling
--- assets/js/score.js
+++ assets/js/score.js
@@ -1,7 +1,3 @@
-var finalScore = window.sessionStorage.getItem("FinalScore");
-
-window.sessionStorage.removeItem("FinalScore");
-
 const scoresStorageKey = "JSGauntletScores";
 
 const scoreTableElement = document.querySelector("#score-table-body"); // Technically this is just the body of the table, but this is where we add our data.
@@ -9,8 +5,25 @@ const submitScoreForm = document.querySelector("#submit-score-form");
 const initialsInputElement = document.querySelector("#initials-input");
 const clearScoresButtonElement = document.querySelector("#clear-scores");
 
+var finalScore;
 var scoreTableEntries = [];
 
+/**
+ * Initializes page for displaying previous scores
+ */
+function init()
+{
+    finalScore = window.sessionStorage.getItem("FinalScore");
+    window.sessionStorage.removeItem("FinalScore");
+    loadScoresFromStorage();
+    populateScoreTable();
+}
+
+/**
+ * Adds a score table entry (consisting of initials and score) to the table
+ * @param {*} initials the entry's initials
+ * @param {*} score the entry's score
+ */
 function addToScoreTable(initials, score)
 {
     var newItem = document.createElement("div");
@@ -29,6 +42,9 @@ function addToScoreTable(initials, score)
     scoreTableElement.appendChild(newItem);
 }
 
+/**
+ * Refresh the elements on the page to correspond to the score entries in local memory
+ */
 function populateScoreTable()
 {
     for(var i = 0; i < scoreTableEntries.length; i++)
@@ -37,12 +53,19 @@ function populateScoreTable()
     }
 }
 
+/**
+ * Remove all elements inside the score table element
+ */
 function clearScoreTable()
 {
     scoreTableElement.replaceChildren([]);
 }
 
-// Saves score to storage
+/**
+ * Creates a score table entry and writes it to local storage
+ * @param {*} initials the entry's initials
+ * @param {*} score the entry's score
+ */
 function saveScoreToStorage(initials, score)
 {
     scoreTableEntries.push({
@@ -52,6 +75,9 @@ function saveScoreToStorage(initials, score)
     window.localStorage.setItem(scoresStorageKey, JSON.stringify(scoreTableEntries))
 }
 
+/**
+ * Load scores from browser's local storage into local memory
+ */
 function loadScoresFromStorage()
 {
     scoreTableEntries = JSON.parse(window.localStorage.getItem(scoresStorageKey));
@@ -61,10 +87,9 @@ function loadScoresFromStorage()
     }
 }
 
-loadScoresFromStorage();
-
-populateScoreTable();
-
+/**
+ * Adds a submit listener to the score form to save to browser's local storage and updates display
+ */
 submitScoreForm.addEventListener("submit", function(event)
 {
     event.preventDefault(); // default behavior is to reload page
@@ -75,6 +100,9 @@ submitScoreForm.addEventListener("submit", function(event)
     submitScoreForm.style.visibility = "hidden"; // Oh, you wanted to spam the scoreboard with your score through multiple subissions? I don't think so!
 });
 
+/**
+ * Adds a click listener to the "Clear Scores" button that clears the entries in the browser's local storage and updates the display
+ */
 clearScoresButtonElement.addEventListener("click", function()
 {
     scoreTableEntries = [];
@@ -83,4 +111,6 @@ clearScoresButtonElement.addEventListener("click", function()
     clearScoreTable();
     loadScoresFromStorage();
     populateScoreTable();
-})
+});
+
+init();
